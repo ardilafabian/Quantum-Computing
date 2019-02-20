feec2e8cbbfdb109c421b72dbd23f5f8cfed6686
Fabian Ardila Rodriguez <fabianra97@gmail.com>
2019-02-19 21:18:32 -0500

    Prepared all algebra operations

--- src/complexnumberslibrary/ComplexOperation.java
+++ src/complexnumberslibrary/ComplexOperation.java
@@ -32,4 +32,66 @@ public class ComplexOperation {
         
     }
     
+    public ComplexNumber complexNumberMultiplication(ComplexNumber cn1, ComplexNumber cn2) {
+        
+        double rp1 = cn1.getRealPart();
+        double ip1 = cn1.getImaginaryPart();
+        
+        double rp2 = cn2.getRealPart();
+        double ip2 = cn2.getImaginaryPart();
+        
+        double resultRp = (rp1*rp2) - (ip1*ip2); 
+        double resultIp = (rp1*ip2) + (rp2*ip1);
+        
+        return new ComplexNumber(resultRp, resultIp);
+        
+    }
+    
+    public ComplexNumber complexNumberRest(ComplexNumber cn1, ComplexNumber cn2) {
+        
+        double rp1 = cn1.getRealPart();
+        double ip1 = cn1.getImaginaryPart();
+        
+        double rp2 = cn2.getRealPart();
+        double ip2 = cn2.getImaginaryPart();
+        
+        double resultRp = rp1 - rp2; 
+        double resultIp = ip1 - ip2;
+        
+        return new ComplexNumber(resultRp, resultIp);
+    }
+    
+    public ComplexNumber complexNumberDivision(ComplexNumber cn1, ComplexNumber cn2) {
+        
+        double rp1 = cn1.getRealPart();
+        double ip1 = cn1.getImaginaryPart();
+        
+        double rp2 = cn2.getRealPart();
+        double ip2 = cn2.getImaginaryPart();
+        
+        double resultRp = ((rp1 * rp2) + (ip1 * ip2)) / (Math.pow(rp2, 2) + Math.pow(ip2, 2)); 
+        double resultIp = ((rp2 * ip1) - (rp1 * ip2)) / (Math.pow(rp2, 2) + Math.pow(ip2, 2));
+        
+        return new ComplexNumber(resultRp, resultIp);
+        
+    }
+    
+    public double complexNumberModulus(ComplexNumber cn) {
+        
+        double rp = cn.getRealPart();
+        double ip = cn.getImaginaryPart();
+        
+        return Math.sqrt((Math.pow(rp, 2) + Math.pow(ip, 2)));
+        
+    }
+    
+    public ComplexNumber complexNumberConjugation(ComplexNumber cn) {
+        
+        double rp = cn.getRealPart();
+        double ip = cn.getImaginaryPart();
+        
+        return new ComplexNumber(rp, ip * -1);
+        
+    }
+    
 }
--- src/complexnumberslibrary/Main.java
+++ src/complexnumberslibrary/Main.java
@@ -16,9 +16,37 @@ public class Main {
      */
     public static void main(String[] args) {
         
-        ComplexNumber cp1 = new ComplexNumber(0, 0);
+        ComplexOperation op = new ComplexOperation();
         
-        System.out.println("Numero complejo: " + cp1);
+        ComplexNumber cp1;
+        ComplexNumber cp2;
+        
+        cp1 = new ComplexNumber(3, -1);
+        cp2 = new ComplexNumber(1, 4);
+        
+        System.out.println("--- Test Case 1: ---\n     c1  = " + cp1 + "\n     c2 = " + cp2 + "\n\n");
+        
+        System.out.println("Sum -> c1 + c2 = " + op.complexNumberSum(cp1, cp2) + "\n");
+        System.out.println("Multiplication -> c1 + c2 = " + op.complexNumberMultiplication(cp1, cp2) + "\n");
+        System.out.println("Rest -> c1 + c2 = " + op.complexNumberRest(cp1, cp2) + "\n\n");
+        
+        /*-------------------------------------------------------------------------*/
+        
+        cp1 = new ComplexNumber(-2, 1);
+        cp2 = new ComplexNumber(1, 2);
+        
+        System.out.println("--- Test Case 2: ---\n     c1  = " + cp1 + "\n     c2 = " + cp2 + "\n\n");
+        
+        System.out.println("Division -> c1 + c2 = " + op.complexNumberDivision(cp1, cp2) + "\n\n");
+        
+        /*-------------------------------------------------------------------------*/
+        
+        cp1 = new ComplexNumber(1, -1);
+        
+        System.out.println("--- Test Case 3: ---\n     c  = " + cp1 + "\n\n");
+        
+        System.out.println("Modulus -> |c| = " + op.complexNumberModulus(cp1) + "\n");
+        System.out.println("Conjugation -> c = " + op.complexNumberConjugation(cp1) + "\n\n");
         
     }
     
