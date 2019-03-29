3bd1a2f1e19fc6523d5d6f6364133785fc0a8ef1
Fabián Ardila <fabianra97@gmail.com>
2019-03-29 16:16:48 -0500

    Add test of multiple dynamics

--- test/ComplexTest/ComplexMarblesTest.java
+++ test/ComplexTest/ComplexMarblesTest.java
@@ -9,6 +9,7 @@ import complexnumberslibrary.ComplexMatrix;
 import complexnumberslibrary.ComplexNumber;
 import complexnumberslibrary.ComplexVector;
 import complexnumberslibrary.MarblesExperiment;
+import java.util.ArrayList;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -68,5 +69,54 @@ public class ComplexMarblesTest {
         ComplexVector res = MarblesExperiment.getNewState();
         
         assertTrue("ComplexMarblesSimulation failed.", res.equals(expected));
+    }
+    
+    @Test
+    public void MultipleComplexSimulation() throws Exception {
+        ComplexVector cv1;
+        ComplexVector expected;
+        
+        ComplexMatrix cmx1 = new ComplexMatrix();
+        
+        cv1 = new ComplexVector();
+        cv1.addComplexNumber(new ComplexNumber(0.7071, 0));
+        cv1.addComplexNumber(new ComplexNumber(0.7071, 0));
+        cv1.addComplexNumber(new ComplexNumber(0, 0));
+        cmx1.addComplexEntry(cv1);
+        
+        cv1 = new ComplexVector();
+        cv1.addComplexNumber(new ComplexNumber(-0.7071, 0));
+        cv1.addComplexNumber(new ComplexNumber(-0.7071, 0));
+        cv1.addComplexNumber(new ComplexNumber(0, 0));
+        cmx1.addComplexEntry(cv1);
+        
+        cv1 = new ComplexVector();
+        cv1.addComplexNumber(new ComplexNumber(0, 0));
+        cv1.addComplexNumber(new ComplexNumber(0, 0));
+        cv1.addComplexNumber(new ComplexNumber(0, 1));
+        cmx1.addComplexEntry(cv1);
+        
+        ComplexVector state = new ComplexVector();
+        state.addComplexNumber(new ComplexNumber(0.5774, 0));
+        state.addComplexNumber(new ComplexNumber(0, 5164));
+        state.addComplexNumber(new ComplexNumber(0, 0.6325));
+        
+        MarblesExperiment marbles1 = new MarblesExperiment(state);
+        
+        ArrayList<ComplexMatrix> dynamicSet1 = new ArrayList<>();
+        dynamicSet1.add(cmx1);
+        dynamicSet1.add(cmx1);
+        dynamicSet1.add(cmx1);
+        
+        ComplexVector res1 = marbles1.dynamicSimulation(dynamicSet1);
+        
+        //-----------------------------
+         
+        MarblesExperiment marbles2 = new MarblesExperiment(cmx1, state, 3);
+        ComplexVector res2 = marbles2.getNewState();
+        
+        
+        assertEquals("MultipleComplexSimulation", true , res1.equals(res2));
+        assertEquals("MultipleComplexSimulation", true , res2.equals(res1));
     }
 }
